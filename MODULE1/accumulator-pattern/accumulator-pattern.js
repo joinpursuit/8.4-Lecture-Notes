9afdad7fbb72e0d716017328696a1b9d32d092d8
tim mckiernan <tim@pursuit.org>
2022-02-02 20:42:46 -0500

    updates accumulator

--- MODULE1/accumulator-pattern/accumulator-pattern.js
+++ MODULE1/accumulator-pattern/accumulator-pattern.js
@@ -25,8 +25,8 @@
 function sumAllNumbers(numbers) {
   let sum = 0;
   for (let i = 0; i < numbers.length; i++ ) {
-    // sum += numbers[i];
     sum = sum + numbers[i];
+    // sum += numbers[i];  is another way of writing this!
   }
   return sum;
 }
@@ -63,8 +63,11 @@ console.log(presentAllStates([]));
   Write a function that determines whether or not any of the 
   states provided include a space (i.e. " ") in their name.
 */
-//  loop through states once at a time
-//  then loop AGAIN and check each character in the string for a space;
+// Let's solve this using NESTED LOOPS!  
+//  What are our steps to solve?
+//   1.  loop through states once at a time
+//   2.  then loop AGAIN and check each character in the string for a space;
+//   3.  If we find a string - return true;  If not we run to the end of loop and return false!
 function hasSpace(states) {
   let spaces = false;
   for (let i = 0; i < states.length; i++ ) {
@@ -84,49 +87,82 @@ function hasSpace(states) {
 console.log(hasSpace(["Alaska", "New York", "Florida"])); //> true
 console.log(hasSpace(["Alaska", "Montana", "Florida"])); //> false
 
-/*
-  IS VALID
-  ---------------
-  Write a function that returns false if any state abbreviation is longer than two characters.
-*/
-function isValid(states) {}
-
-console.log(isValid(["AK", "NYC", "FL"])); //> false
-console.log(isValid(["AK", "WA", "FL"])); //> true
 
 /*
-  KEBAB CASE
-  ---------------
-  Write a function that returns a new array of all the states in kebab casing.
+KEBAB CASE
+---------------
+Write a function that returns a new array of all the states in kebab casing.
 */
+// Lets solve this without the .replace() string method!
 function kebabCase(states) {
   
+  // Loop through the first array 
   for (let i = 0; i < states.length; i++) {
+    // For EVERY index in the array - loop through and check the individual characters
     for (let j = 0 ; j < states[i].length; j ++) {
+      // check the ARRAY at index [i] and the CHARACTER in that string  at index [j] to equal " "!
       if (states[i][j] === " ") {
-          states[i] = states[i].split("");
-          states[i][j] = "-";
-          states[i] = states[i].join("")   
+        // split the string to an array - save to a variable
+        let split  = states[i].split("");
+        // change the index of " " to "-"
+        split[j] = "-";
+        // change states[i] to our new string returned from .join()
+        states[i] = split.join("")   
+        
+        // THIS ALSO WORKS!   SO MANY WAYS TO SOLVE THIS PROBLEM!
         // let split = states[i].split(" ");
         // let joined = split[0].concat("-").concat(split[1]);
         // states[i] = joined;
       }
     }
   }
+  // this array constructor makes a NEW ARRAY from our states array
   return Array.from(states);
 }
 
 console.log(kebabCase(["Alaska", "New York", "Florida"]));
-//> [ "alaska", "new-york", "florida" ]
+//> [ "Alaska", "New-York", "Florida" ]
 console.log(kebabCase([]));
 //> []
 
 /*
+  IS VALID
+  ---------------
+  Write a function that returns false if ANY state abbreviation is longer than two characters.
+*/
+function isValid(states) {
+  let onlyTwo = true;
+  for (let i = 0 ; i < states.length; i++) {
+    if (states[i].length > 2) {
+      onlyTwo = false;
+      break;
+      // return false also works and IS BETTER but for today - let's do it long form
+    }
+  }
+  return onlyTwo
+
+}
+
+console.log(isValid(["AK", "NYC", "FL"])); //> false
+console.log(isValid(["AK", "WA", "FL"])); //> true
+
+/*
   FIND
   ---------------
-  Write a function that looks for a state by name. If that state is found, return the state name. If it is not found, return `null`.
+  Write a function that looks for a state by name. If that state is found, return the state name. 
+  If it is not found, return `null`.
 */
-function find(states, name) {}
+function find(states, name) {
+  let foundState = null;
+  for (let i = 0; i < states.length; i++) {
+    if (states[i] === name) {
+      foundState = name;
+      break;
+      // return name
+    }
+  }
+  return foundState;
+}
 
 console.log(find(["Alaska", "New York", "Florida"], "Alaska")); //> "Alaska"
 console.log(find(["Alaska", "New York", "Florida"], "Montana")); //> null
@@ -134,11 +170,20 @@ console.log(find(["Alaska", "New York", "Florida"], "Montana")); //> null
 /*
   FILTER ABBREVIATIONS
   ---------------
-  Write a function that filters out all strings equal to or longer than 3 characters. Return a new array with just the valid abbreviations.
+  Write a function that filters out ALL strings equal to or longer than 3 characters. 
+  Return a new array with just the valid abbreviations.
 */
-function filterAbbreviations(states) {}
+function filterAbbreviations(states) {
+  let valid = [];
+  for (let i = 0; i < states.length; i++) {
+    if (states[i].length < 3) {
+      valid.push(states[i]);
+    }
+  }
+  return valid;
+}
 
-console.log(filterAbbreviations(["AK", "MT", "WA", "NYC"]));
+console.log(filterAbbreviations(["AK", "MT", "WA", "NYC","AK", "MT", "WA", "NYC","AK", "MT", "WA", "NYC"]));
 //> [ "AK", "MT", "WA" ]
 console.log(filterAbbreviations(["Alaska", "New York", "Florida"]));
 //> []
