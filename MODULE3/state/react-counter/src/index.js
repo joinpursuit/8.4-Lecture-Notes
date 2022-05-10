d593b284339a919505d0e0291ed9f4ac58ccba73
gscarborough <gigimscarborough@gmail.com>
2022-05-09 20:11:32 -0400

    add notes with state setup

--- MODULE3/state/react-counter/src/index.js
+++ MODULE3/state/react-counter/src/index.js
@@ -6,7 +6,10 @@ import "./styles.css";
 class Counter extends React.Component {
   constructor() {
     super();
-    this.count = 0
+
+    this.state = {
+      count: 0
+    }
     // We define this.state inside the constructor function and set it equal to and object
     // IMPORTANT: OUR STATE SHOULD ALWAYS BE AN OBJECT!!!!!
     
@@ -16,16 +19,17 @@ class Counter extends React.Component {
   // THIS IS OUR EVENT HANDLER
   handleClick = () => {
     
-    this.count += 1
-    
+    this.setState({
+      count: ++this.state.count
+    })
     // once we've created our state lets update our count by incrementing it by 1 in this event handler using `this.setState` when a user clicks the button
     // Remember! That this.setState must take in an object with key value pairs corresponding to the slice of state we want to update
 
   };
 
   render() {
-    //CRITICAL THINKING: Why can I destructure cunt from this as seen below?
-    const { count } = this;
+    //CRITICAL THINKING: Why can I destructure count from this as seen below?
+    const { count } = this.state;
     // lets set count to a variable here by destructuring state instead of this
     return (
       <div>
