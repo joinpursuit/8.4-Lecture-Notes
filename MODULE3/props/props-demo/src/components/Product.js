640b4a7de6273c47cbcc7749841c087361a618aa
tim mckiernan <tim@pursuit.org>
2022-05-11 15:42:58 -0400

    adds forms boilerplate

--- MODULE3/props/props-demo/src/components/Product.js
+++ MODULE3/props/props-demo/src/components/Product.js
@@ -17,3 +17,22 @@ const { name, price, description } = props
     </div>
   );
 }
+
+// export class Propduct extends React.Component {
+//   render() {
+//     const styleObj = {
+//       border: "2px solid green",
+//       padding: "25px",
+//       width: "50%",
+//     };
+//     const { name, price, description } = this.props
+//     return (
+//       <div style={ styleObj }>
+//         <h3>
+//           { name } - { price }
+//         </h3>
+//         <p>Desc: { description }</p>
+//       </div>
+//     );
+//   }
+// }
--- MODULE3/props/props-demo/src/components/Products.js
+++ MODULE3/props/props-demo/src/components/Products.js
@@ -34,3 +34,43 @@ export default function Products() {
     </div>
   );
 }
+
+
+ // class verison 
+// export class Products extends React.Component {
+//   constructor(){
+//     this.prods = [
+//     { name:"newItem",id:"adwawa16837426", price:1, description:"this will make everything rerender" },
+//     { name: "stuff",id:"adwawa1", price: 100, description: "pretty good" },
+//     { name: "other",id:"adwawa2", price: 1020, description: "pretty bad" },
+//     { name: "stuff1",id:"adwawa3", price: 102430, description: "pretty good!!!!!!!" },
+//     { name: "stuff2",id:"adwawa4", price: 1400, description: "terrible" },
+//     { name: "stuff3", id:"adwawa5", price: 1030, description: "the book was better" },
+//     {
+//       name: "stuff4",
+//       price: 1060,
+//       description: "I really like this I cant tell you why",
+//     },
+//   ];
+//   }
+//   render(){
+//     return (
+//       <div>
+//         <h2>Products are below</h2>
+//         <ul>
+//           { prods.map((prod) => {
+//             return (
+//               <li key = { prod.id }> 
+//                 <Product
+//                   name={prod.name}
+//                   price={prod.price}
+//                   description={prod.description}
+//                 />
+//               </li>
+//             );
+//           }) }
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
