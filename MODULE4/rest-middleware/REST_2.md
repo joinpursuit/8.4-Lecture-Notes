efdae278d1aea54668927dd5712131248babea25
Gigi Scarborough <gigimscarborough@gmail.com>
2022-07-18 20:20:29 -0400

    add update and delete

--- MODULE4/rest-middleware/REST_2.md
+++ MODULE4/rest-middleware/REST_2.md
@@ -9,10 +9,13 @@
 What is REST?
 
 What is CRUD?
+    - Create, Read, Update, Delete : operations to interact with our data
 
 What are the REST actions?
+    - Create, Index, Show, Update, Destroy
 
 What routes have we made so far?
+    - Index, Show, .....?
 
 What REST Actions do our routes use?
 
--- MODULE4/rest-middleware/bookmark-app/controllers/bookmarksController.js
+++ MODULE4/rest-middleware/bookmark-app/controllers/bookmarksController.js
@@ -7,10 +7,11 @@ const { validateURL } = require("../models/validations")
 // quick reminder!  We can key into arrays with strings because they are objects!
 console.log(bookmarksData["1"]);
 // in order to even get to this line the path is already "/bookmarks"
+// INDEX ROUTE
 bookmarks.get("/", (req, res) => {
     res.json(bookmarksData);
 });
-
+// SHOW ROUTE
 bookmarks.get("/:arrayIndex", (req, res) => {
     console.log(req.params);
     const { arrayIndex } = req.params;
@@ -21,12 +22,23 @@ bookmarks.get("/:arrayIndex", (req, res) => {
     } 
 })
 
-
+// CREATE ROUTE
 bookmarks.post("/", validateURL, (req, res) => {
     bookmarksData.push(req.body);
     res.json(bookmarksData[bookmarksData.length - 1]);
 });
 
+bookmarks.delete("/:arrayIndex", (req, res)=> {
+    const {arrayIndex} = req.params
+    const deletedBookmark = bookmarksData.splice(arrayIndex, 1)
+    res.status(200).json(deletedBookmark)
+});
+
+bookmarks.put("/:arrayIndex", (req,res) => {
+    const {arrayIndex} = req.params
+    bookmarksData[arrayIndex] = req.body
+    res.status(200).json(bookmarksData[arrayIndex])
+});
 
 
 
